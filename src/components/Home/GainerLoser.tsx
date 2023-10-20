229990c14cb2b7f16b278aeda67dd3f08ed73d3c
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-10-21 00:21:12 +0530

    Both gainer and loosers section done

---
+++ src/components/Home/GainerLoser.tsx
@@ -0,0 +1,85 @@
+"use client";
+import React, { useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { Card, CardContent, CardHeader } from "../ui/card";
+import { GainerLoser } from "../../../types/types";
+const GainerLoser = ({ GainerData }: any) => {
+  const [selectedValue, setSelectedValue] = useState("price");
+  return (
+    <Card className="w-full">
+      <CardHeader className="w-full border-b mb-4">
+        <div className="flex justify-between items-center">
+          <span className="text-sm font-thin">TICKER</span>
+          <div className="hidden sm:flex w-1/2 justify-between">
+            <span className="text-sm font-thin">PRICE</span>
+            <span className="text-sm font-thin">CHANGE AMOUNT</span>
+            <span className="text-sm font-thin">CHANGE PERCENTAGE</span>
+            <span className="text-sm font-thin">VOLUME</span>
+          </div>
+          <div className="flex sm:hidden w-1/3 justify-between">
+            <Select
+              defaultValue={selectedValue}
+              onValueChange={(value) => setSelectedValue(value)}
+            >
+              <SelectTrigger className="w-full text-[12px]">
+                <SelectValue placeholder="Select" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Top Gainers</SelectLabel>
+                  <SelectItem value="price">PRICE</SelectItem>
+                  <SelectItem value="change_amount">CHANGE AMOUNT</SelectItem>
+                  <SelectItem value="change_percentage">
+                    CHANGE PERCENTAGE
+                  </SelectItem>
+                  <SelectItem value="volume">VOLUME</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+      </CardHeader>
+      {GainerData?.map(
+        ({
+          ticker,
+          price,
+          change_amount,
+          change_percentage,
+          volume,
+        }: GainerLoser) => (
+          <CardContent key={ticker}>
+            <div className="flex justify-between items-center">
+              <span className="text-sm font-medium text-primary">{ticker}</span>
+              <div className="hidden sm:flex w-1/2 justify-between text-center">
+                <span className="text-sm font-medium">${price}</span>
+                <span className="text-sm font-medium">{change_amount}</span>
+                <span className="text-sm font-medium">{change_percentage}</span>
+                <span className="text-sm font-medium">{volume}</span>
+              </div>
+              <div className="flex sm:hidden w-full justify-between text-end">
+                <span className="text-sm font-medium">
+                  {selectedValue === "price" && <>${price}</>}
+                  {selectedValue === "change_amount" && change_amount}
+                  {selectedValue === "change_percentage" && (
+                    <span className="text-primary">{change_percentage}%</span>
+                  )}
+                  {selectedValue === "volume" && volume}
+                </span>
+              </div>
+            </div>
+          </CardContent>
+        )
+      )}
+    </Card>
+  );
+};
+
+export default GainerLoser;
--- src/components/Home/TabContainer.tsx
+++ src/components/Home/TabContainer.tsx
@@ -1,21 +1,8 @@
 "use client";
 import { useTopGainerLoser } from "@/utils/Hooks/useTopGainerLoser";
-import React, { useState } from "react";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "../ui/tabs";
-import { Card, CardContent, CardHeader } from "../ui/card";
-import { GainerLoser } from "../../../types/types";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "../ui/select";
 
 const TabContainer = () => {
-  const [selectedValue, setSelectedValue] = useState("price");
   const { data: GainerLoser } = useTopGainerLoser();
   console.log("data", GainerLoser);
 
@@ -26,82 +13,10 @@ const TabContainer = () => {
         <TabsTrigger value="loser">Losers</TabsTrigger>
       </TabsList>
       <TabsContent value="gainer" className="w-full">
-        <Card className="w-full ">
-          <CardHeader className="w-full border-b mb-4">
-            <div className="flex justify-between items-center">
-              <span className="text-sm font-thin">TICKER</span>
-              <div className="hidden sm:flex w-1/2 justify-between">
-                <span className="text-sm font-thin">PRICE</span>
-                <span className="text-sm font-thin">CHANGE AMOUNT</span>
-                <span className="text-sm font-thin">CHANGE PERCENTAGE</span>
-                <span className="text-sm font-thin">VOLUME</span>
-              </div>
-              <div className="flex sm:hidden w-1/3 justify-between">
-                <Select
-                  defaultValue={selectedValue}
-                  onValueChange={(value) => setSelectedValue(value)}
-                >
-                  <SelectTrigger className="w-full text-[12px]">
-                    <SelectValue placeholder="Select" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectGroup>
-                      <SelectLabel>Top Gainers</SelectLabel>
-                      <SelectItem value="price">PRICE</SelectItem>
-                      <SelectItem value="change_amount">
-                        CHANGE AMOUNT
-                      </SelectItem>
-                      <SelectItem value="change_percentage">
-                        CHANGE PERCENTAGE
-                      </SelectItem>
-                      <SelectItem value="volume">VOLUME</SelectItem>
-                    </SelectGroup>
-                  </SelectContent>
-                </Select>
-              </div>
-            </div>
-          </CardHeader>
-          {GainerLoser?.top_gainers?.map(
-            ({
-              ticker,
-              price,
-              change_amount,
-              change_percentage,
-              volume,
-            }: GainerLoser) => (
-              <CardContent key={ticker}>
-                <div className="flex justify-between items-center">
-                  <span className="text-sm font-medium text-primary">
-                    {ticker}
-                  </span>
-                  <div className="hidden sm:flex w-1/2 justify-between text-center">
-                    <span className="text-sm font-medium">${price}</span>
-                    <span className="text-sm font-medium">{change_amount}</span>
-                    <span className="text-sm font-medium">
-                      {change_percentage}
-                    </span>
-                    <span className="text-sm font-medium">{volume}</span>
-                  </div>
-                  <div className="flex sm:hidden w-1/2 justify-between text-end">
-                    <span className="text-sm font-medium">
-                      {selectedValue === "price" && <>${price}</>}
-                      {selectedValue === "change_amount" && change_amount}
-                      {selectedValue === "change_percentage" && (
-                        <span className="text-primary">
-                          {change_percentage}%
-                        </span>
-                      )}
-                      {selectedValue === "volume" && volume}
-                    </span>
-                  </div>
-                </div>
-              </CardContent>
-            )
-          )}
-        </Card>
+        <GainerLoser GainerData={GainerLoser?.top_gainers} />
       </TabsContent>
       <TabsContent value="loser" className="w-full">
-        <Card className="w-full"></Card>
+        <GainerLoser GainerData={GainerLoser?.top_losers} />
       </TabsContent>
     </Tabs>
   );
