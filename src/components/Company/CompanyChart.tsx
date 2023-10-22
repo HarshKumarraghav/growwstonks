a81e92bc5d21139a655c3ede909262c14f696206
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-10-23 01:32:21 +0530

    Mobile view fixed

--- src/components/Company/CompanyChart.tsx
+++ src/components/Company/CompanyChart.tsx
@@ -51,7 +51,7 @@ const CompanyChart = ({ Symbol }: CompanyChartProps) => {
           FilteredStockData={FilteredStockData}
           ChartType={chartType}
         />
-        <div className=" flex w-full mt-10 justify-between items-center">
+        <div className=" flex flex-col sm:fle-row w-full mt-10 justify-between items-center">
           <div className="flex justify-center">
             <button
               className={`p-2 shadow-md border rounded-l-md w-14 ${
